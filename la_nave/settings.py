ce26d7b0d209349c0c42bb36a33bc22d9d6be97f
Jothanf <jothanferney@gmail.com>
2024-12-23 21:16:38 -0500

    Refactor settings.py to streamline database configuration by consolidating the DATABASE_URL setup into a single line. This change enhances readability and maintains the integration of environment variables for database management.

--- la_nave/settings.py
+++ la_nave/settings.py
@@ -83,9 +83,7 @@ WSGI_APPLICATION = 'la_nave.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # DATABASES = {
